bcd59b31b410555129eb68bd8960aa2c79e7368d
Nick <bagotnj@gmail.com>
2022-07-21 22:25:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -32,19 +32,20 @@ def is_empty():
 
 
 def clicked():
-    web = website_entry.get()
-    em = email_entry.get()
-    pas = password_entry.get()
-    if len(web) == 0 or len(pas) == 0:
+    my_website = website_entry.get()
+    my_email = email_entry.get()
+    my_password = password_entry.get()
+    if len(my_website) == 0 or len(my_password) == 0 or len(my_email) == 0:
         is_empty()
     else:
-        messagebox.askokcancel(title=f"{web}",
-                               message=f"Here's the information you entered:\n {web} , {em} , {pas}\n Is it okay to "
-                                       f"save?")
+        messagebox.askokcancel(title=f"{my_website}",
+                               message=f"Here's the information you entered:\n {my_website} , {my_email} , {my_password}"
+                                       f"\n Is it okay to save?")
         data = open("data.txt", "a")
-        data.write(f"({web} , {em} , {pas})\n")
+        data.write(f"({my_website} , {my_email} , {my_password})\n")
         data.close()
         website_entry.delete(0, END)
+        email_entry.delete(0,END)
         password_entry.delete(0, END)
 
 
@@ -53,7 +54,7 @@ window = Tk()
 window.title("Password Manager")
 window.configure(padx=30, pady=30)
 canvas = Canvas(width=200, height=200)
-logo_img = PhotoImage(file="C:/Users/Nick/Downloads/password-manager/lock.png")
+logo_img = PhotoImage(file="lock.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
@@ -69,7 +70,6 @@ email.grid(column=0, row=2)
 
 email_entry = Entry(width=52)
 email_entry.grid(column=1, row=2, columnspan=2)
-email_entry.insert(0, "fake_email@gmail.com")
 
 password = Label(text="Password:")
 password.grid(column=0, row=3)
@@ -84,3 +84,4 @@ add = Button(text="Add", width=44, command=clicked)
 add.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
+
